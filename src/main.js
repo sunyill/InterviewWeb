a6eb788d76d2b36c4aa239161d7480c9b181dad5
wb135 <wb135@yingu.com>
2019-09-01 23:12:25 +0800

    2.新建login页及home页

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式"
+Vue.use(ElementUi) // 注入全局组件
 
 Vue.config.productionTip = false
 
